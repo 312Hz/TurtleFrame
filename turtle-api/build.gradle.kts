ff015bf165662f579e195a17b5592deba333bf5d
hanhan2001 <764932129@qq.com>
2025-07-26 23:09:19 +0800

    feat: turtle-api 导入 afybroker

--- turtle-api/build.gradle.kts
+++ turtle-api/build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
 }
 
 dependencies {
+    // AfyBroker
+    compileOnly("net.afyer.afybroker:afybroker-core:2.5")
+    compileOnly("net.afyer.afybroker:afybroker-client:2.5")
     // spigot-api
     compileOnly("org.spigotmc:spigot-api:1.21-R0.1-SNAPSHOT")
 }
