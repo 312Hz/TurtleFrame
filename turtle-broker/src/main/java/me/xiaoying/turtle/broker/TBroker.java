2b8a529cfdc439e42641b50a982d76929db83240
hanhan2001 <764932129@qq.com>
2025-07-26 15:12:29 +0800

    feat: 支持数据库配置

--- turtle-broker/src/main/java/me/xiaoying/turtle/broker/TBroker.java
+++ turtle-broker/src/main/java/me/xiaoying/turtle/broker/TBroker.java
@@ -1,22 +1,30 @@
 package me.xiaoying.turtle.broker;
 
 import me.xiaoying.sqlfactory.SqlFactory;
-import me.xiaoying.turtle.api.option.Option;
+import me.xiaoying.sqlfactory.config.MysqlConfig;
+import me.xiaoying.sqlfactory.config.PostgreSqlConfig;
+import me.xiaoying.sqlfactory.config.SqliteConfig;
+import me.xiaoying.sqlfactory.factory.MysqlFactory;
+import me.xiaoying.sqlfactory.factory.PostgreSqlFactory;
+import me.xiaoying.sqlfactory.factory.SqliteFactory;
+import me.xiaoying.turtle.api.core.TCore;
 import me.xiaoying.turtle.api.option.OptionManager;
 import me.xiaoying.turtle.broker.command.PluginsCommand;
 import me.xiaoying.turtle.broker.file.FileManager;
 import me.xiaoying.turtle.broker.file.SimpleFileManager;
 import me.xiaoying.turtle.broker.file.TBrokerFile;
 import me.xiaoying.turtle.broker.option.SimpleOptionManager;
+import me.xiaoying.turtle.broker.processor.OptionClassificationRequestProcessor;
 import me.xiaoying.turtle.broker.processor.OptionRequestProcessor;
 import net.afyer.afybroker.server.plugin.Plugin;
+
+import java.io.File;
+import java.util.Locale;
 
 public class TBroker extends Plugin {
     private static FileManager fileManager;
     private static OptionManager optionManager;
     private static Plugin instance;
-
-    private static SqlFactory sqlFactory;
 
     @Override
     public void onLoad() {
@@ -25,6 +33,7 @@ public class TBroker extends Plugin {
         // processors
         System.out.println("[*] 注册 Processors .");
         this.getServer().registerUserProcessor(new OptionRequestProcessor());
+        this.getServer().registerUserProcessor(new OptionClassificationRequestProcessor());
     }
 
     @Override
@@ -36,6 +45,22 @@ public class TBroker extends Plugin {
         TBroker.fileManager = new SimpleFileManager();
         TBroker.fileManager.register(new TBrokerFile());
         TBroker.fileManager.loads();
+
+        // sqlfactory
+        SqlFactory sqlFactory;
+        switch (TBrokerFile.DATABASE_TYPE.toUpperCase(Locale.ENGLISH)) {
+            case "MYSQL":
+                sqlFactory = new MysqlFactory(new MysqlConfig(TBrokerFile.DATABASE_MYSQL_USERNAME, TBrokerFile.DATABASE_MYSQL_PASSWORD, TBrokerFile.DATABASE_MYSQL_HOST, TBrokerFile.DATABASE_MYSQL_PORT, TBrokerFile.DATABASE_MYSQL_DATABASE));
+                break;
+            case "POSTGRESQL":
+                sqlFactory = new PostgreSqlFactory(new PostgreSqlConfig(TBrokerFile.DATABASE_POSTGRESQL_USERNAME, TBrokerFile.DATABASE_POSTGRESQL_PASSWORD, TBrokerFile.DATABASE_POSTGRESQL_HOST, TBrokerFile.DATABASE_POSTGRESQL_PORT, TBrokerFile.DATABASE_POSTGRESQL_DATABASE));
+                break;
+            case "SQLITE":
+            default:
+                sqlFactory = new SqliteFactory(new SqliteConfig(new File(TBrokerFile.DATABASE_SQLITE_FILE)));
+                break;
+        }
+        TCore.setSqlFactory(sqlFactory);
 
         // commands
         System.out.println("[*] 注册命令.");
@@ -49,6 +74,8 @@ public class TBroker extends Plugin {
 
     @Override
     public void onDisable() {
+        TBroker.getOptionManager().save();
+
         System.out.println("[#] " + this.getDescription().getName() + " 已卸载.");
     }
 
