5b1918b55e4bf57b086160ecf59b6a48ffcbab32
hanhan2001 <764932129@qq.com>
2025-07-29 14:25:53 +0800

    feat: 修改模块编译顺序

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "TurtleFrame"
-include("turtle-common")
 include("turtle-api")
+include("turtle-common")
 include("turtle-broker")
 include("turtle-bukkit")
 include("turtle-web")
